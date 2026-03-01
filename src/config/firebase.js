1eb9ca89e9bc70241265027d7acb1c6d5ae14a53
phamminhlocse184295 <locpmse184295@fpt.edu.vn>
2026-03-01 12:30:48 +0700

    feat: thay thế Firebase bằng Cloudinary, hỗ trợ upload dữ liệu đa phương thức (Image/Audio/Text/Video)

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLF3OWcdFbLOo_GjHR5gjE-c5yoHRXfsM",
+  authDomain: "nhom5topicfe.firebaseapp.com",
+  projectId: "nhom5topicfe",
+  storageBucket: "nhom5topicfe.firebasestorage.app",
+  messagingSenderId: "266003184159",
+  appId: "1:266003184159:web:416f73983ecf9a0720732a",
+  measurementId: "G-301EM7BDJ2",
+};
+
+// Khởi tạo Firebase
+const app = initializeApp(firebaseConfig);
+
+// Khởi tạo Storage và export ra để dùng
+export const storage = getStorage(app);
